9537faebd16444bb3c166146c1cdb53871dad5c9
Andrey ``Bass'' Shcheglov <andrewbass@gmail.com>
2016-10-27 19:55:16 +0300

    Convert to JUnit4 tests

--- src/test/java/org/hamcrest/text/pattern/internal/naming/GroupNamespaceTest.java
+++ src/test/java/org/hamcrest/text/pattern/internal/naming/GroupNamespaceTest.java
@@ -1,49 +1,60 @@
 package org.hamcrest.text.pattern.internal.naming;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class GroupNamespaceTest extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class GroupNamespaceTest {
     GroupNamespace environment = new GroupNamespace();
 
+    @Test
     public void testBindsIndicesToNamesAllocatingIndicesFromOne() {
         environment.create("a");
         environment.create("b");
 
-        assertEquals(1, (int) environment.resolve("a"));
-        assertEquals(2, (int) environment.resolve("b"));
+        assertEquals(1, environment.resolve("a"));
+        assertEquals(2, environment.resolve("b"));
     }
 
+    @Test
     public void testThrowsIllegalArgumentExceptionIfNameNotBound() {
         try {
             environment.resolve("not bound");
             fail("expected IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
+        } catch (@SuppressWarnings("unused") final IllegalArgumentException e) {
             // expected
         }
     }
 
+    @Test
     public void testThrowsIllegalArgumentExceptionIfDuplicateNameBound() {
         environment.create("a");
         try {
             environment.create("a");
             fail("expected IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
+        } catch (@SuppressWarnings("unused") final IllegalArgumentException e) {
             // expected
         }
     }
 
+    @Test
     public void testLooksForNameInParentEnvironmentIfNotFoundLocally() {
         environment.create("a");
-        GroupNamespace b = environment.create("b");
+        final GroupNamespace b = environment.create("b");
 
         assertEquals(environment.resolve("a"), b.resolve("a"));
     }
 
+    @Test
     public void testAllocatesIndicesForAndResolvesPathsWithinChildEnvironments() {
-        GroupNamespace a = environment.create("a");
+        final GroupNamespace a = environment.create("a");
         a.create("x");
         a.create("y");
-        GroupNamespace b = environment.create("b");
+        final GroupNamespace b = environment.create("b");
         b.create("x");
         b.create("y");
 
@@ -55,13 +66,14 @@ public class GroupNamespaceTest extends TestCase {
         assertEquals(environment.resolve(new Path("b", "y")), a.resolve(new Path("b", "y")));
     }
 
+    @Test
     public void testThrowsIllegalArgumentExceptionIfMiddleOfPathNotBound() {
         environment.create("a");
 
         try {
             environment.resolve(new Path("a", "b", "c"));
             fail("should have thrown IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
+        } catch (@SuppressWarnings("unused") final IllegalArgumentException e) {
             // expected
         }
     }
--- src/test/java/org/hamcrest/text/pattern/internal/naming/PathTest.java
+++ src/test/java/org/hamcrest/text/pattern/internal/naming/PathTest.java
@@ -1,8 +1,17 @@
 package org.hamcrest.text.pattern.internal.naming;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-public class PathTest extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class PathTest {
+    @Test
+    @SuppressWarnings("static-method")
     public void testCanBeComparedForEquality() {
         assertTrue(new Path("a", "b").equals(new Path("a", "b")));
         assertFalse(new Path("a", "b").equals(new Path("a")));
@@ -15,14 +24,16 @@ public class PathTest extends TestCase {
         assertTrue(new Path("a", "b").equals(new Path("x", "a", "b").tail()));
     }
 
+    @Test
+    @SuppressWarnings("static-method")
     public void testReturnsLengthAndComponents() {
-        Path abc = new Path("a", "b", "c");
+        final Path abc = new Path("a", "b", "c");
         assertEquals(3, abc.size());
         assertEquals("a", abc.component(0));
         assertEquals("b", abc.component(1));
         assertEquals("c", abc.component(2));
 
-        Path xy = new Path("x", "y");
+        final Path xy = new Path("x", "y");
         assertEquals(2, xy.size());
         assertEquals("x", xy.component(0));
         assertEquals("y", xy.component(1));
@@ -30,22 +41,30 @@ public class PathTest extends TestCase {
         assertEquals(0, new Path().size());
     }
 
+    @Test
+    @SuppressWarnings("static-method")
     public void testReturnsHead() {
         assertEquals("a", new Path("a", "b", "c", "d").head());
         assertEquals("x", new Path("x", "y", "z").head());
     }
 
+    @Test
+    @SuppressWarnings("static-method")
     public void testReturnsTail() {
         assertEquals(new Path("b", "c", "d"), new Path("a", "b", "c", "d").tail());
         assertEquals(new Path("y", "z"), new Path("x", "y", "z").tail());
         assertEquals(new Path(), new Path("a").tail());
     }
 
+    @Test
+    @SuppressWarnings("static-method")
     public void testParsesPathFromDottedNotation() {
         assertEquals(new Path("a"), Path.parse("a"));
         assertEquals(new Path("a", "b", "c"), Path.parse("a.b.c"));
     }
 
+    @Test
+    @SuppressWarnings("static-method")
     public void testReturnsDottedPathNotationFromToString() {
         assertEquals("a", new Path("a").toString());
         assertEquals("a.b.c", new Path("a", "b", "c").toString());
